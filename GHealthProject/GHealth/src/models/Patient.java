aa1268e1700c5b42ee2a29d974b0f0372b284c01
obaida mnasra <obaida mnasra>
2016-05-12 13:22:16 +0300

    2 class patient reff

---
+++ GHealthProject/GHealth/src/models/Patient.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Patient {
+	private String nameOfHMO; 
+	private int HMOcode ; 
+	private float weight; 
+	private float hight ; 
+	private String gender; 
+	private String EmailOfHMO; 
+	private  ArrayList<Referral> Referrals; 
+	
+	
+}
---
+++ GHealthProject/GHealth/src/models/Referral.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.sql.Date;
+
+public class Referral {
+	private int Id ; 
+	private String DoctorName ; 
+	private Date date ; 
+	private String Description; 
+	private String Specially ; 
+	private Patient patient ; 
+	
+	
+	
+
+}
