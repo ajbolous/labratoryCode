d717b02a04e8a174867b38bc7dfbb54006b674d3
Muhamad Igbaria <Muhamad Igbaria>
2016-05-20 03:14:58 +0300

    no message

--- GHealthProject/GClient/src/ClientUI/AddUserUI.java
+++ GHealthProject/GClient/src/ClientUI/AddUserUI.java
@@ -31,6 +31,8 @@ import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.DropMode;
+import javax.swing.DefaultComboBoxModel;
 
 public class AddUserUI {
 
@@ -40,8 +42,6 @@ public class AddUserUI {
 	private JTextField textField_3;
 	private JTextField textField_5;
 	private JTextField textField_6;
-	private JPasswordField passwordField;
-	private JTextField textField_1;
 
 	
 	public AddUserUI() {
@@ -73,84 +73,85 @@ public class AddUserUI {
 		template.getContentPane().add(logo);
 		
 		JLabel lblNewLabel = new JLabel("ID:");
-		lblNewLabel.setBounds(29, 111, 114, 35);
+		lblNewLabel.setBounds(29, 106, 114, 35);
 		template.getContentPane().add(lblNewLabel);
 		
-		JLabel label = new JLabel("Password:");
-		label.setBounds(29, 143, 114, 35);
-		template.getContentPane().add(label);
-		
 		JLabel label_1 = new JLabel("First Name:");
-		label_1.setBounds(29, 175, 114, 35);
+		label_1.setBounds(29, 136, 114, 35);
 		template.getContentPane().add(label_1);
 		
 		JLabel label_2 = new JLabel("Last Name:");
-		label_2.setBounds(29, 207, 114, 35);
+		label_2.setBounds(29, 166, 114, 35);
 		template.getContentPane().add(label_2);
 		
 		JLabel label_3 = new JLabel("Birth Date:");
-		label_3.setBounds(29, 238, 59, 35);
+		label_3.setBounds(29, 196, 59, 35);
 		template.getContentPane().add(label_3);
 		
 		JLabel label_4 = new JLabel("Email:");
-		label_4.setBounds(29, 269, 114, 35);
+		label_4.setBounds(29, 226, 114, 35);
 		template.getContentPane().add(label_4);
 		
 		JLabel label_5 = new JLabel("Phone:");
-		label_5.setBounds(29, 299, 114, 35);
+		label_5.setBounds(29, 256, 114, 35);
 		template.getContentPane().add(label_5);
 		
 		textField = new JTextField();
-		textField.setBounds(90, 115, 252, 26);
+		textField.setBounds(90, 110, 252, 26);
 		template.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(90, 179, 252, 26);
+		textField_2.setBounds(90, 140, 252, 26);
 		template.getContentPane().add(textField_2);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(90, 211, 252, 26);
+		textField_3.setBounds(90, 170, 252, 26);
 		template.getContentPane().add(textField_3);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(90, 273, 252, 26);
+		textField_5.setBounds(90, 230, 252, 26);
 		template.getContentPane().add(textField_5);
 		
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
-		textField_6.setBounds(90, 303, 252, 26);
+		textField_6.setBounds(90, 260, 252, 26);
 		template.getContentPane().add(textField_6);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(90, 147, 252, 26);
-		template.getContentPane().add(passwordField);
 		
 		JButton btnNewButton = new JButton("Save");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(162, 338, 89, 23);
+		btnNewButton.setBounds(162, 297, 89, 23);
 		template.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancel");
-		btnNewButton_1.setBounds(253, 338, 89, 23);
+		btnNewButton_1.setBounds(253, 297, 89, 23);
 		template.getContentPane().add(btnNewButton_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(90, 242, 252, 26);
-		template.getContentPane().add(textField_1);
+		JComboBox year_cbox = new JComboBox();
+		year_cbox.setBackground(Color.WHITE);
+		year_cbox.setModel(new DefaultComboBoxModel(new String[] {"Year"}));
+		year_cbox.setBounds(89, 203, 72, 20);
+		template.getContentPane().add(year_cbox);
 		
-		JLabel label_6 = new JLabel("(DD/MM/YYYY)");
-		label_6.setBounds(352, 237, 73, 35);
-		template.getContentPane().add(label_6);
+		JComboBox mounth_cbox = new JComboBox();
+		mounth_cbox.setBackground(Color.WHITE);
+		mounth_cbox.setModel(new DefaultComboBoxModel(new String[] {"Mounth", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		mounth_cbox.setBounds(171, 203, 80, 20);
+		template.getContentPane().add(mounth_cbox);
+		
+		JComboBox day_cbox = new JComboBox();
+		day_cbox.setBackground(Color.WHITE);
+		day_cbox.setModel(new DefaultComboBoxModel(new String[] {"Day"}));
+		day_cbox.setBounds(262, 203, 80, 20);
+		template.getContentPane().add(day_cbox);
 		template.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{logo}));
-		template.setBounds(100, 100, 431, 408);
+		template.setBounds(100, 100, 428, 370);
 		template.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
