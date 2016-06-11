33a9b73485d8450d421b450133fc4a84f08a8a93
maisam <mar.m94@hotmail.com>
2016-06-11 22:55:54 +0300

    add java doc and delete classes

--- GHealthProject/GHealth/src/models/Manager.java
+++ GHealthProject/GHealth/src/models/Manager.java
@@ -3,6 +3,7 @@ package models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+
 @DatabaseTable(tableName = "managers")
 public class Manager extends ClinicEmployee {
 	@DatabaseField()
